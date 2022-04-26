5f1a6a22c04f3928d47cc3d9c7d4f50195200f81
Naveenkumar M <72449129+NaveenkumarMD@users.noreply.github.com>
2022-04-26 22:08:42 +0530

    form values

--- src/Screens/Officials/Officiallogin.jsx
+++ src/Screens/Officials/Officiallogin.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React,{useState} from 'react'
 import '../../Styles/signup.css'
 import SVG from '../../Icons/login.svg'
 import Button from '../../Components/Button'
 import { Link } from 'react-router-dom'
 function Officiallogin() {
+    const [email,setemail]=useState("")
+    const [password,setpassword]=useState("")
+    const submit=()=>{
+        alert("done")
+    }
     return (
         <div className='signup-container'>
             <div className='carousel'>
@@ -31,21 +36,23 @@ function Officiallogin() {
                         <div>
                             <div className='label'>Email</div>
                             <div>
-                                <input className='input' placeholder='Enter your mail' />
+                                <input className='input' placeholder='Enter your mail' value={email} onChange={e=>setemail(e.target.value)} />
                             </div>
                         </div>
                         <div>
-                            <div className='label'>Email</div>
+                            <div className='label'>Password</div>
                             <div>
-                                <input className='input' placeholder='Enter your mail' />
+                                <input className='input' type="password" placeholder='Enter your password' value={password} onChange={e=>setpassword(e.target.value)} />
                             </div>
                         </div>
                     </div>
 
                     <div style={{ marginTop: "40px" }}></div>
-                    <Button text="Login" style={{ width: "20px" }} />
+                    <div onClick={submit}>
+                    <Button text="Login"  style={{ width: "20px" }} />
+                    </div>
                     <div style={{ color: "#CCCCCC", fontSize: "14px", marginTop: "20px" }}>
-                        New to archive?<span className='link'><Link to="/users" classname="link" style={{ textDecoration: "none", color: "#2766F1" }}>Signup</Link></span>
+                        New to archive?<span className='link'><Link to="/user/signup" classname="link" style={{ textDecoration: "none", color: "#2766F1" }}>Signup</Link></span>
                     </div>
                 </div>
             </div>
--- src/Screens/Officials/Officialsignup.jsx
+++ src/Screens/Officials/Officialsignup.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React,{useState} from 'react'
 import '../../Styles/signup.css'
 import SVG from '../../Icons/login.svg'
 import Button from '../../Components/Button'
 import { Link } from 'react-router-dom'
 function Officialsignup() {
+    const [name, setname] = useState('')
+    const [email, setemail] = useState('')
+    const [password, setpassword] = useState('')
+    const [confirmpassword, setconfirmpassword] = useState('')
+    const [phone, setphone] = useState('')
+    const [country, setcountry] = useState('')
+    const submit = () => {
+        alert("sone")
+        if (password !== confirmpassword) {
+            return alert("Password not matched")
+        }
+
+    }
     return (
         <div className='signup-container'>
             <div className='carousel'>
@@ -29,15 +42,15 @@ function Officialsignup() {
                     </div>
                     <div className='content-row'>
                         <div>
-                            <div className='label'>Email</div>
+                            <div className='label'>Name</div>
                             <div>
-                                <input className='input' placeholder='Enter your mail' />
+                                <input className='input' placeholder='Enter your name' value={name} onChange={e => setname(e.target.value)} />
                             </div>
                         </div>
                         <div>
                             <div className='label'>Email</div>
                             <div>
-                                <input className='input' placeholder='Enter your mail' />
+                                <input className='input' placeholder='Enter your mail' value={email} onChange={e => setemail(e.target.value)} />
                             </div>
                         </div>
 
@@ -45,15 +58,15 @@ function Officialsignup() {
                     </div>
                     <div className='content-row'>
                         <div>
-                            <div className='label'>Email</div>
+                            <div className='label'>Mobile</div>
                             <div>
-                                <input className='input' placeholder='Enter your mail' />
+                                <input className='input' placeholder='Enter your number' value={phone} onChange={e => setphone(e.target.value)} />
                             </div>
                         </div>
                         <div>
-                            <div className='label'>Email</div>
+                            <div className='label'>Country</div>
                             <div>
-                                <input className='input' placeholder='Enter your mail' />
+                                <input className='input' placeholder='Enter your Country' value={country} onChange={e => setcountry(e.target.value)} />
                             </div>
                         </div>
 
@@ -61,25 +74,26 @@ function Officialsignup() {
                     </div>
                     <div className='content-row'>
                         <div>
-                            <div className='label'>Email</div>
+                            <div className='label'>Password</div>
                             <div>
-                                <input className='input' placeholder='Enter your mail' />
+                                <input className='input' type="password" placeholder='Enter your password' value={password} onChange={e => setpassword(e.target.value)} />
                             </div>
                         </div>
                         <div>
-                            <div className='label'>Email</div>
+                            <div className='label'>Connfirm password</div>
                             <div>
-                                <input className='input' placeholder='Enter your mail' />
+                                <input className='input'type="password" placeholder='Enter your password again' value={confirmpassword} onChange={e => setconfirmpassword(e.target.value)} />
                             </div>
                         </div>
 
 
                     </div>
-                    <div style={{marginTop:"40px"}}></div>
-                    <Button text="Signup" />
-                    <div style={{color:"#CCCCCC",fontSize:"14px",marginTop:"20px"}}>
-                        Already have an account?<span className='link'><Link to="/users" classname="link" style={{textDecoration:"none",color:"#2766F1"}}>Login</Link></span>
-                        </div>
+                    <div style={{ marginTop: "40px" }}></div>
+                    <div onClick={submit}>
+                        <Button text="Signup" /></div>
+                    <div style={{ color: "#CCCCCC", fontSize: "14px", marginTop: "20px" }}>
+                        Already have an account?<span className='link'><Link to="/user/login" classname="link" style={{ textDecoration: "none", color: "#2766F1" }}>Login</Link></span>
+                    </div>
                 </div>
             </div>
         </div>
