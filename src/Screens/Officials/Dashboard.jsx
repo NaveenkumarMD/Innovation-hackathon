34a201478a6c3c170bcaca3cbdcd6471238e6cb0
SanthoshM25 <54148035+SanthoshM25@users.noreply.github.com>
2022-04-27 01:48:34 +0530

    UI: add dash board card

--- src/Screens/Officials/Dashboard.jsx
+++ src/Screens/Officials/Dashboard.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import Dashboardcard from "../../Components/DashboardCard";
+
+import Navbar from "../../Components/Navbar";
 
 function Dashboard() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <Navbar />
+      <Dashboardcard />
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
