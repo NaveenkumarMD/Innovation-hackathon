88bf81c1a0338a2f624c06501862f75b73234901
SanthoshM25 <54148035+SanthoshM25@users.noreply.github.com>
2022-04-30 01:21:39 +0530

    Update DashboardCard.jsx

--- src/Components/DashboardCard.jsx
+++ src/Components/DashboardCard.jsx
@@ -1,81 +1,107 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
+import { db } from "../firebase";
+import { getDoc, doc } from "firebase/firestore";
 import "../Styles/dashboardCard.css";
-import { MdExpandMore } from 'react-icons/md'
+import { MdExpandMore } from "react-icons/md";
+
 const Dashboardcard = () => {
-  const testData = {
-    tableTitle: "Submitted Documents",
-    tableCols: ["DocId", "SubmittedBy", "Officer"],
-    tableData: [
-      {
-        DocId: 234,
-        SubmittedBy: "user 1",
-        Officer: "Captain Naveen",
-      },
-      {
-        DocId: 234,
-        SubmittedBy: "user 1",
-        Officer: "Captain Naveen",
-      },
-      {
-        DocId: 234,
-        SubmittedBy: "user 1",
-        Officer: "Captain Naveen",
-      },
-    ],
+  const [receivedDocs, setReceivedDocs] = useState([]);
+  const [signedDocs, setSignedDocs] = useState([]);
+  const [rejectedDocs, setRejectedDocs] = useState([]);
+  let docDetails;
+
+  const fetchData = async () => {
+    try {
+      const docsData = await getDoc(
+        doc(db, "docs", localStorage.getItem("email"))
+      );
+      if (docsData.exists() && docsData.data().userDocs.length > 0) {
+        docDetails = docsData.data().userDocs;
+        setReceivedDocs(
+          docDetails.filter(
+            (doc) => !doc.isOpened && !doc.isSigned && !doc.isRejected
+          )
+        );
+        setSignedDocs(
+          docDetails.filter(
+            (doc) => doc.isOpened && doc.isSigned && !doc.isRejected
+          )
+        );
+        setRejectedDocs(
+          docDetails.filter(
+            (doc) => doc.isOpened && !doc.isSigned && doc.isRejected
+          )
+        );
+      }
+    } catch (err) {
+      alert("Can't fetch data at the moment");
+    }
   };
 
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-
     <div className="qcontainer">
-      <div className="qtitle">
-        Recieved documents
-      </div>
-      <div className="qcontent">
-        <Card name="Document 1" sno="1" />
-        <Card name="Document 2" sno="2" />
-        <Card name="Document 3" sno="3" />
-        <div className="qmore">
-          More
-          <MdExpandMore size={24} color="#555" />
+      <div className="qtitle">Recieved documents</div>
+      {receivedDocs.length > 0 && (
+        <div className="qcontent">
+          {receivedDocs.map(
+            (doc, index) =>
+              index < 3 && <Card name={doc.name} sno={doc.docId} />
+          )}
+          <div className="qmore">
+            More
+            <MdExpandMore size={24} color="#555" />
+          </div>
         </div>
+      )}
+      {receivedDocs.length > 0 || (
+        <div style={{ margin: "2em" }}>No Received Documents Found</div>
+      )}
+      <div className="qtitle">Signed documents</div>
+      {signedDocs.length > 0 && (
+        <div className="qcontent">
+          {signedDocs.map(
+            (doc, index) =>
+              index < 3 && <Card name={doc.name} sno={doc.docId} />
+          )}
 
-      </div>
-      <div className="qtitle">
-        Signed documents
-      </div>
-      <div className="qcontent">
-        <Card name="Document 1" sno="1" />
-        <Card name="Document 2" sno="2" />
-        <Card name="Document 3" sno="3" />
-        <div className="qmore">
-          More
-          <MdExpandMore size={24} color="#555" />
+          <div className="qmore">
+            More
+            <MdExpandMore size={24} color="#555" />
+          </div>
         </div>
+      )}
+      {signedDocs.length > 0 || (
+        <div style={{ margin: "2em" }}>No Signed Documents Found</div>
+      )}
+      <div className="qtitle">Rejected documents</div>
+      {rejectedDocs.length > 0 && (
+        <div className="qcontent">
+          {rejectedDocs.map(
+            (doc, index) =>
+              index < 3 && <Card name={doc.name} sno={doc.docId} />
+          )}
 
-      </div>
-      <div className="qtitle">
-        Rejected documents
-      </div>
-      <div className="qcontent">
-      <Card sno={1} name="Documentx"/>
-      <Card sno={2} name="Documentx"/>
-      <Card sno={3} name="Documentx"/>
-   
-        <div className="qmore">
-          More
-          <MdExpandMore size={24} color="#555" />
+          <div className="qmore">
+            More
+            <MdExpandMore size={24} color="#555" />
+          </div>
         </div>
-
-      </div>
+      )}
+      {rejectedDocs.length > 0 || (
+        <div style={{ margin: "2em" }}>No Rejected Documents Found</div>
+      )}
     </div>
   );
 };
 
 export default Dashboardcard;
 
-const Card = ({name,sno}) => {
+const Card = ({ name, sno }) => {
   return (
     <div className="qcard">
       <div className="qleft">
@@ -84,5 +110,5 @@ const Card = ({name,sno}) => {
       </div>
       <div className="qbutton">View</div>
     </div>
-  )
-}
+  );
+};
